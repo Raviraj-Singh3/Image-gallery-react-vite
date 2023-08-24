2ccff9118c885ec8887da9b42457e8f358b12a75
Raviraj-Singh3 <iamtheraviraj@gmail.com>
2023-08-24 15:21:45 +0530

    routing working in local

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>,
-  <div>
-    hello charlie 123 check
-  </div>
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  
+  
 )
