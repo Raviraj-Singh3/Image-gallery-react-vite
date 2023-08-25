e0b639564f8f5ce55b5b073aadd3b7836d4c4aca
Raviraj-Singh3 <iamtheraviraj@gmail.com>
2023-08-25 15:28:12 +0530

    added some css in heading

--- src/components/ImageGallary/ImageGallary.jsx
+++ src/components/ImageGallary/ImageGallary.jsx
@@ -7,7 +7,7 @@ function ImageGallary(){
     const [imageList, setImageList] = useState([])
 
     async function fetchdata(){
-        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=20');
+        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=120');
         // console.log(response.data.photos);
         const results = response.data.photos;
         // const resultPromise = results.map((result) => axios.get(result.url));
@@ -30,22 +30,24 @@ function ImageGallary(){
     
 
     return( 
-        <div className='text-center font-serif text-2xl text-white p-5 bg-blue-500'>
-            <h1>
-                Images
-            </h1>
-        
-    <div className=' flex  '>
-        <div 
-        className=' m-2 flex flex-wrap justify-center bg-green-400
+        <div className='  p-5 bg-green-400'>
+            <div className=' w-auto flex justify-center'>
+                <div className='rounded-md w-1/2 text-center p-4 bg-gray-100'>
+                    <h1 className='text-2xl font-bold text-orange-300'>Images</h1>
+                </div>
+                
+            </div>
+            <div className=' flex  '>
+                
+                <div
+                    className=' m-2 flex flex-wrap justify-center 
          mt-6   '
-        >
-        {imageList.map((i)=> <ImageView  url={i.url} key={i.id} title={i.title} id={i.id} />
-         )}
+                >
+                    {imageList.map((i) => <ImageView url={i.url} key={i.id} title={i.title} id={i.id} />
+                    )}
+                </div>
+            </div>
         </div>
-        
-    </div>
-    </div>
     )
 }
 export default ImageGallary;
--- src/components/ImageView/ImageView.jsx
+++ src/components/ImageView/ImageView.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 function ImageView({url, id}){
     return(
-        <div className="m-3 w-1/5 bg-yellow-400 cursor-pointer hover:bg-orange-100">
+        <div className="m-3 w-1/5 cursor-pointer  ">
             <Link to={`photos/${id}`}>
                 
                 <img src={url} alt="photos" 
-                className="rounded-2xl max-w-full"
+                className="rounded-2xl max-w-full hover:scale-95"
             />
             </Link>
             
