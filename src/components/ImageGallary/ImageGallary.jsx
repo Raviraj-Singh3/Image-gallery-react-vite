2516153ca826d6ce0037d95d223b390a4841c0d0
Raviraj-Singh3 <iamtheraviraj@gmail.com>
2023-08-26 15:37:47 +0530

    next button added and logic : next until 132

--- src/components/ImageGallary/ImageGallary.jsx
+++ src/components/ImageGallary/ImageGallary.jsx
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import ImageView from '../ImageView/ImageView';
+// https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20
 
 function ImageGallary(){
 
-    const [imageList, setImageList] = useState([])
-
+    const [imageList, setImageList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [offset, setOffset] = useState(0)
+    const [photosLink, setPhotosLink] = useState(`https://api.slingacademy.com/v1/sample-data/photos?offset=${offset}&limit=12`)
+    
     async function fetchdata(){
-        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=120');
-        // console.log(response.data.photos);
+        const response = await axios.get(photosLink);
         const results = response.data.photos;
-        // const resultPromise = results.map((result) => axios.get(result.url));
-        // const resultData = await axios.all(resultPromise);
+        console.log(results);
+        // setOffset( offset + 10);
         const res = results.map((result) => {
             return {
                 id: result.id,
@@ -19,16 +22,16 @@ function ImageGallary(){
                 url: result.url
             }
         });
-        console.log(res);
         setImageList(res);
+        setIsLoading(false);
+        setOffset(offset+12)
     }
     
 
     useEffect(() => {
         fetchdata();
-    },[])
+    },[photosLink])
     
-
     return( 
         <div className='  p-5 bg-green-400'>
             <div className=' w-auto flex justify-center'>
@@ -40,10 +43,29 @@ function ImageGallary(){
             <div className='  '>
                 
                 <div className='grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 mt-6'>
+
                     {imageList.map((i) => <ImageView url={i.url} key={i.id} title={i.title} id={i.id} />
                     )}
+                    
                 </div>
+                
             </div>
+            <div className='items-center flex justify-center'>
+                {   (offset < 132) 
+                        && 
+                    <button className='bg-white px-7 py-2 rounded-md disabled:opacity-50'
+                        // disabled={offset > 24}
+                        onClick={()=> { 
+                        setPhotosLink(`https://api.slingacademy.com/v1/sample-data/photos?offset=${offset}&limit=12`);
+                        console.log("inside click", offset)
+                        }}> 
+                        Next
+                    </button>
+                }
+                </div>
+                <div>
+                    {(isLoading) && 'Loading' }
+                </div>
         </div>
     )
 }
